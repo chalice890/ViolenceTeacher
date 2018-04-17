81e5721d31ddeeefb181589f6b8506ead614640a
Chalice <tatsuki-k-17e@stu.kbc.ac.jp>
2018-04-17 13:44:37 +0900

    [途中保存]二日目午前(プレイヤー移動・当たり判定、障害物、タイマー)

---
+++ Project-VT/Assets/Scenes/Tatsuki/LimitTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LimitTimer : MonoBehaviour {
+
+    public float counttime = 60;  
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counttime -= Time.deltaTime;
+        GetComponent<Text>().text = counttime.ToString("F2");
+	}
+}
---
+++ Project-VT/Assets/Scenes/Tatsuki/PlayerControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed = 0.1f;
+    public int flg = 0; 
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        flg = -1;
+        Debug.Log("Block!!");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        flg = 1;
+        Debug.Log("Block!!");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -speed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed,0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+
+
+
+    }
+}
