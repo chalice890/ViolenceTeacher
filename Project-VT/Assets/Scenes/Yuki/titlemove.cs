0941fb007ca599c8a20516925bf603de6d0a540d
LAPTOP-UEIRJFST\moyo0 <yuki-t-17d@stu.kbc.ac.jp>
2018-04-18 15:02:57 +0900

    titleシーンを作成

---
+++ Project-VT/Assets/Scenes/Yuki/titlemove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titlemove : MonoBehaviour {
+
+    public GameObject Titleimg;
+    public GameObject Titleimg2;
+    public float movex;
+    static public int titleflg = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Titleimg.transform.position.x < 0 )
+        {
+            Titleimg.transform.position += new Vector3(movex, 0, 0);
+        }
+        else
+        {
+            titleflg = 1;
+        }
+        if (titleflg == 1){
+            if (Titleimg2.transform.position.x > 0)
+            {
+                Titleimg2.transform.position -= new Vector3(movex, 0, 0);
+            }
+        }
+    }
+}
