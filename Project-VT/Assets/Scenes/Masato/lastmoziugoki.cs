4b20bcf0a98acdc9f5dd9cd723851775c958004d
LAPTOP-DDJN77VS\KBC用 <masato-y-17e@stu.kbc.ac.jp>
2018-04-18 09:41:23 +0900

    [途中保存]二日目終了時点データ

---
+++ Project-VT/Assets/Scenes/Masato/lastmoziugoki.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lastmoziugoki : MonoBehaviour {
+
+    public GameObject StaffImg;
+    public float movey = 0.2f;
+    public float endTime = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StaffImg.transform.position.y < 0)
+        {
+            StaffImg.transform.position += new Vector3(0, movey, 0);
+        }
+
+        endTime -= Time.deltaTime;
+
+        if (endTime < 0)
+        {
+            Application.Quit();
+        }
+    }
+
+}
---
+++ Project-VT/Assets/Scenes/Masato/moziugoki.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moziugoki : MonoBehaviour {
+
+
+    public GameObject StaffImg;
+    public float movey = 0.2f;
+
+    // Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	void Update () {
+            StaffImg.transform.position += new Vector3(0, movey, 0);
+    }
+
+}
